9d8d9635017441499d1a1586c90f836da8d233c7
WhatABbas <aabbas@stuy.edu>
2017-01-05 12:49:46 -0500

    Went through Classic/Misere and fixed spacing (improved readability). Created XsNOs, which will become the main file for this project. The original intent was to use aCardLayout to organize all the games, but that's proving to be difficult. For now, a flow layout with buttons that show the different games in a new window is in place.

--- GridTest.java
+++ GridTest.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;//NEW STUFF!
 import java.awt.event.*;
 import java.util.Arrays;
-public class ClassicTicTacToe extends JFrame {
+public class TTTGrid extends JFrame {
     private boolean XorO = true;
     private boolean gameWon= false;
     //true is x false is o
@@ -26,108 +26,102 @@ public class ClassicTicTacToe extends JFrame {
 
     public  void  checkforwinner(String[][]array){
 	    for(int i=0; i<array.length;i++){
-    		if(!array[i][0].equals("")&& array[i][0].equals(array[i][1]) && array[i][0].equals(array[i][2])){
-                System.out.println(array[i][0]+ " won the game!");
-                gameWon=true;
-    		}
+		if(!array[i][0].equals("")&& array[i][0].equals(array[i][1]) && array[i][0].equals(array[i][2])){
+		    System.out.println(array[i][0]+ " won the game!");
+		     gameWon=true;
 
-    		if(!array[0][i].equals("")&& array[0][i].equals(array[1][i]) && array[0][i].equals(array[2][i])){
-    		    System.out.println(array[0][i]+ " won the game!");
-    		     gameWon=true;
-    		}
-        }
-    	if(!array[0][0].equals("")&& array[0][0].equals(array[1][1]) && array[0][0].equals(array[2][2])){
-            System.out.println(array[0][0]+ " won the game!");
-            gameWon=true;
-        }
-        if(!array[0][2].equals("")&& array[0][2].equals(array[1][1]) && array[0][2].equals(array[2][0])){
-            System.out.println(array[0][2]+ " won the game!");
-            gameWon=true;
-        }
-    }
-
+		}
+		if(!array[0][i].equals("")&& array[0][i].equals(array[1][i]) && array[0][i].equals(array[2][i])){
+		    System.out.println(array[0][i]+ " won the game!");
+		     gameWon=true;
+		}}
+		if(!array[0][0].equals("")&& array[0][0].equals(array[1][1]) && array[0][0].equals(array[2][2])){
+		    System.out.println(array[0][0]+ " won the game!");
+		    gameWon=true;}
+		if(!array[0][2].equals("")&& array[0][2].equals(array[1][1]) && array[0][2].equals(array[2][0])){
+		     System.out.println(array[0][2]+ " won the game!");
+		     gameWon=true;
+		}}
     public void placeletter(int row, int col, JButton button){
-        if(!gameWon){
-            if (button.getText().equals("-")){
-                if(XorO){
-                    button.setText("X");
-                    valUs[row][col] = "X";
-                }
-            else{
-                button.setText("O");
-                valUs[row][col] = "O";
+	if(!gameWon){
+	if (button.getText().equals("-")){
+		    if(XorO){
+			button.setText("X");
+			valUs[row][col] = "X";
 		    }
-            XorO = !XorO;
+			    else{
+			button.setText("O");
+			valUs[row][col] = "O";
+		    }
+		    XorO = !XorO;
 		    checkforwinner(valUs);
-            }
-        }
-    }
+	}}}
 
-    public ClassicTicTacToe(){
+    public TTTGrid(){
         this.setTitle("Tic Tac Toe");
         this.setSize(1000,1000);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         pane1 = this.getContentPane();
         pane1.setLayout(new GridLayout(3,3));
-	    for(int i=0; i<valUs.length;i++){
-            for(int x=0; x<valUs[i].length;x++){
-                valUs[i][x]="";
-            }
-        }
+	for(int i=0; i<valUs.length;i++){
+	    for(int x=0; x<valUs[i].length;x++){
+		valUs[i][x]="";}}
+
 
         JButton UL = new JButton("-");
-        UL.addActionListener(new ActionListener(){
+        UL.addActionListener(new ActionListener()
+        {
             public void actionPerformed(ActionEvent event){
-                placeletter(0,0,UL);}
-        });
+		placeletter(0,0,UL);}
+	    });
 
         JButton UC = new JButton("-");
-        UC.addActionListener(new ActionListener(){
+        UC.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event){
-                placeletter(0,1,UC);}
+		placeletter(0,1,UC);}
+
         });
 
         JButton UR = new JButton("-");
         UR.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event){
-                placeletter(0,2,UR);}
+		placeletter(0,2,UR);}
         });
 
         JButton ML = new JButton("-");
         ML.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event){
-                placeletter(1,0,ML);}
+		placeletter(1,0,ML);}
         });
 
         JButton MC = new JButton("-");
         MC.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event){
-                placeletter(1,1,MC);}
-        });
+		placeletter(1,1,MC);}
 
+        });
         JButton MR = new JButton("-");
         MR.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event){
-                placeletter(1,2,MR);}
+		placeletter(1,2,MR);}
         });
 
         JButton BL = new JButton("-");
         BL.addActionListener(new ActionListener() {
 	     public void actionPerformed(ActionEvent event){
-             placeletter(2,0,BL);}
+		 placeletter(2,0,BL);}
         });
-
         JButton BC = new JButton("-");
         BC.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event){
-                placeletter(2,1,BC);}
+		placeletter(2,1,BC);}
         });
 
         JButton BR = new JButton("-");
         BR.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event){
-                placeletter(2,2,BR);}
+	        placeletter(2,2,BR);}
         });
 
 
@@ -140,12 +134,10 @@ public class ClassicTicTacToe extends JFrame {
         pane1.add(BL);
         pane1.add(BC);
         pane1.add(BR);
-	}
+	    }
 
     public static void main(String[] args){
-        ClassicTicTacToe g = new ClassicTicTacToe();
+        TTTGrid g = new TTTGrid();
         g.setVisible(true);
     }
-
-
-	    }
+}
---
+++ XsNOs.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class XsNOs extends JFrame implements ActionListener{
+    Container c;
+    ClassicTicTacToe og = new ClassicTicTacToe();
+    MisereTicTacToe rg = new MisereTicTacToe();
+    JButton misere = new JButton("Misere");
+    JButton classic = new JButton("Classic");
+
+    public XsNOs(){
+        c = this.getContentPane();
+        c.setLayout(new FlowLayout());
+        this.setSize(500,300);
+        this.setTitle("Choose your Tic-Tac-Toe");
+
+
+        classic.addActionListener(this);
+
+        misere.addActionListener(this);
+
+        c.add(classic);
+        c.add(misere);
+
+        c.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == classic){
+            og.setVisible(true);
+        }
+        if(e.getSource() == misere){
+            rg.setVisible(false);
+        }
+    }
+
+    public static void main(String[] args){
+        XsNOs game = new XsNOs();
+        game.setVisible(true);
+    }
+}
