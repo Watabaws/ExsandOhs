5562516a96bf9aeba34b04aaab0ac706f6e1132e
WhatABbas <aabbas@stuy.edu>
2017-01-16 14:56:36 -0500

    Added MNK functionality to the GUI

---
+++ MNKGUI.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MNKGUI extends JFrame implements ActionListener{
+    Container pane;
+    JTextField m, n, k;
+    JButton submit;
+    MNK game;
+
+    public MNKGUI(){
+        pane = this.getContentPane();
+        pane.setLayout(new GridLayout(0,4));
+        this.setSize(300,100);
+        this.setTitle("Enter the values for MNK");
+
+        m = new JTextField();
+        n = new JTextField();
+        k = new JTextField();
+
+        pane.add(m);
+        pane.add(n);
+        pane.add(k);
+
+        submit = new JButton("Create Game");
+        submit.addActionListener(this);
+
+        pane.add(submit);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        int em, en, kay;
+        em = Integer.parseInt(m.getText());
+        en = Integer.parseInt(n.getText());
+        kay = Integer.parseInt(k.getText());
+        game = new MNK(em, en, kay);
+        setVisible(false);
+        game.setVisible(true);
+    }
+}
